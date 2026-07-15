108e3f703cf8895681e3f2ecd703f72470b0e45c
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:09:50 +0530

    Create 300-LongestIncreasingSubsequence

---
+++ https:/github.com/lucius-lt/leetcode.git/300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -0,0 +1,22 @@
+// Last updated: 7/16/2026, 1:09:50 AM
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length ==0){
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int ans = 1;
+        for(int i = 1; i<dp.length; i++){
+            int max = 0;
+            for(int j = 0; j<i; j++){
+                if (nums[i] > nums[j]){
+                    max = Math.max(max, dp[j]);
+                }
+            }
+            dp[i] = max + 1;
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+}
