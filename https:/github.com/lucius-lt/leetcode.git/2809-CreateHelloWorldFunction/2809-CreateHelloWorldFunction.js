0a39abcfa1a9839ac1ad92270ade5881e16a3320
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:07:57 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ https:/github.com/lucius-lt/leetcode.git/2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 7/16/2026, 1:07:57 AM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function() {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
