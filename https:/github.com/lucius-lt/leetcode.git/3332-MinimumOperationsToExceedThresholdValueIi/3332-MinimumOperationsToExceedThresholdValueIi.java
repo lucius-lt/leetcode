70ac0ca8886c46fca9e28cc5bbdb40a6804ebdda
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:07:54 +0530

    Create 3332-MinimumOperationsToExceedThresholdValueIi

---
+++ https:/github.com/lucius-lt/leetcode.git/3332-MinimumOperationsToExceedThresholdValueIi/3332-MinimumOperationsToExceedThresholdValueIi.java
@@ -0,0 +1,22 @@
+// Last updated: 7/16/2026, 1:07:54 AM
+class Solution {
+
+    public int minOperations(int[] nums, int k) {
+        PriorityQueue<Long> minHeap = new PriorityQueue<Long>(
+            Arrays.stream(nums)
+                .mapToLong(i -> (long) i)
+                .boxed()
+                .collect(Collectors.toList())
+        );
+        int numOperations = 0;
+
+        while (minHeap.peek() < k) {
+            long x = minHeap.remove();
+            long y = minHeap.remove();
+            minHeap.add(Math.min(x, y) * 2 + Math.max(x, y));
+
+            numOperations++;
+        }
+        return numOperations;
+    }
+}
