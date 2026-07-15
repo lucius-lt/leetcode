ab5516c80b9fcca2fc136108b2bf828f9440bb08
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:09:02 +0530

    Create 792-BinarySearch

---
+++ https:/github.com/lucius-lt/leetcode.git/792-BinarySearch/792-BinarySearch.c
@@ -0,0 +1,17 @@
+// Last updated: 7/16/2026, 1:09:01 AM
+int search(int* nums, int numsSize, int target) {
+     int left =0,right = numsSize-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return -1;
+}
