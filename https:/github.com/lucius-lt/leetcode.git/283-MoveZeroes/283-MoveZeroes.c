49e3ef0283a72867924677847506eb66e49ab8b6
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:10:02 +0530

    Create 283-MoveZeroes

---
+++ https:/github.com/lucius-lt/leetcode.git/283-MoveZeroes/283-MoveZeroes.c
@@ -0,0 +1,13 @@
+// Last updated: 7/16/2026, 1:10:01 AM
+void moveZeroes(int* nums, int numsSize) {
+    int j =0;
+    for(int i =0;i<numsSize;i++){
+        if(nums[i]!=0){
+            nums[j]=nums[i];
+            j++;
+        }
+    }
+    for(int i =j;i<numsSize;i++){
+        nums[i]=0;
+    }
+}
