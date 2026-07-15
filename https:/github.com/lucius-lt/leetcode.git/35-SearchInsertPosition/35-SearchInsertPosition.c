9d4efbb46d8a63e8db75a035302b957331278982
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:12:16 +0530

    Create 35-SearchInsertPosition

---
+++ https:/github.com/lucius-lt/leetcode.git/35-SearchInsertPosition/35-SearchInsertPosition.c
@@ -0,0 +1,14 @@
+// Last updated: 7/16/2026, 1:12:16 AM
+int searchInsert(int* nums, int numsSize, int target) {
+    int left=0,right=numsSize-1;
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]==target){
+            return mid;
+
+        }
+        else if(nums[mid]<target) left=mid+1;
+        else right = mid-1;
+    }
+    return left;
+}
