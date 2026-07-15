30df727b42269f4b9d742c9f8875b6369662086d
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:11:32 +0530

    Create 100-SameTree

---
+++ https:/github.com/lucius-lt/leetcode.git/100-SameTree/100-SameTree.java
@@ -0,0 +1,24 @@
+// Last updated: 7/16/2026, 1:11:32 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null&&q==null) return true;
+        if(p==null||q==null||p.val!=q.val) return false;
+
+        return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+    }
+}
