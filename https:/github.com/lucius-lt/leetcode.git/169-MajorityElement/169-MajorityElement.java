0f345c33bb6ff04065e5e732cad2065ba23073c3
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:10:42 +0530

    Create 169-MajorityElement

---
+++ https:/github.com/lucius-lt/leetcode.git/169-MajorityElement/169-MajorityElement.java
@@ -0,0 +1,15 @@
+// Last updated: 7/16/2026, 1:10:41 AM
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            } }
+        return candidate;}}
