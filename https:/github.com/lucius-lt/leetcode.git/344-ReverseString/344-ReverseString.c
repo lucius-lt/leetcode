8c684dc0be30fe44a84fdf3b2971b9f914a4e798
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:09:47 +0530

    Create 344-ReverseString

---
+++ https:/github.com/lucius-lt/leetcode.git/344-ReverseString/344-ReverseString.c
@@ -0,0 +1,11 @@
+// Last updated: 7/16/2026, 1:09:46 AM
+void reverseString(char* s, int sSize) {
+    int left = 0;
+    int right = sSize-1;
+    while(left <= right){
+        int temp = s[left];
+        s[left++]=s[right];
+        s[right--]=temp;
+   
+    }
+    }
