37119f4c4b16a3ea5d344e8483157d1ba2262ae2
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:12:19 +0530

    Create 34-FindFirstAndLastPositionOfElementInSortedArray

---
+++ https:/github.com/lucius-lt/leetcode.git/34-FindFirstAndLastPositionOfElementInSortedArray/34-FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,12 @@
+// Last updated: 7/16/2026, 1:12:19 AM
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int f=-1;
+        int l=-1;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]==target){
+                if(f==-1){
+                    f=i;
+                }
+                    l=i; }}
+        return new int[]{f,l};  }}
