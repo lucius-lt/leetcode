0d0e1e0ba9630265b43427445f019469374cb7f6
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:10:04 +0530

    Create 242-ValidAnagram

---
+++ https:/github.com/lucius-lt/leetcode.git/242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,15 @@
+// Last updated: 7/16/2026, 1:10:03 AM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int c : count) {
+            if (c != 0) return false;
+        }
+        return true;
+    }
+}
