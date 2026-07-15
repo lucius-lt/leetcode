366224b07d9971006e1a4af0ba0f8bec35647888
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:10:08 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ https:/github.com/lucius-lt/leetcode.git/232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.java
@@ -0,0 +1,47 @@
+// Last updated: 7/16/2026, 1:10:07 AM
+class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public MyQueue() {
+        stack1= new Stack<>();
+        stack2= new Stack<>();
+        
+    }
+    
+    public void push(int x) {
+        stack1.push(x);
+        
+    }
+    
+    public int pop() {
+        if(stack2.isEmpty()) movestack1Tostack2();
+        return stack2.pop();
+    }
+    
+    public int peek() {
+        if(stack2.isEmpty()) movestack1Tostack2();
+        return stack2.peek();
+        
+    }
+    
+    public boolean empty() {
+        return stack1.isEmpty()&&stack2.isEmpty();
+
+        
+    }
+    private void movestack1Tostack2(){
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
