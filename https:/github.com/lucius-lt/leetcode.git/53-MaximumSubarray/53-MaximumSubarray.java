efda5ec8b80af98a4baa7b117643ab92b670bd80
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:12:05 +0530

    Create 53-MaximumSubarray

---
+++ https:/github.com/lucius-lt/leetcode.git/53-MaximumSubarray/53-MaximumSubarray.java
@@ -0,0 +1,15 @@
+// Last updated: 7/16/2026, 1:12:05 AM
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
