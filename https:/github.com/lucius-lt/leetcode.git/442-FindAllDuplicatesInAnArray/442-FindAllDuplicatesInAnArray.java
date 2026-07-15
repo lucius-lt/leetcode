20fa08fe9b960a848d770d69b2bb8049159cc7cf
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:09:39 +0530

    Create 442-FindAllDuplicatesInAnArray

---
+++ https:/github.com/lucius-lt/leetcode.git/442-FindAllDuplicatesInAnArray/442-FindAllDuplicatesInAnArray.java
@@ -0,0 +1,18 @@
+// Last updated: 7/16/2026, 1:09:39 AM
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+
+            if (nums[index] < 0) {
+                result.add(Math.abs(nums[i]));
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+
+        return result;
+    }
+}
