7b7dabec2ff4c86d050c7aa7defe94e3e3fabddb
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:08:51 +0530

    Create 948-SortAnArray

---
+++ https:/github.com/lucius-lt/leetcode.git/948-SortAnArray/948-SortAnArray.java
@@ -0,0 +1,37 @@
+// Last updated: 7/16/2026, 1:08:50 AM
+class Solution {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums,0,nums.length-1);
+        return nums;
+    }
+    void mergeSort(int[] arr, int start, int end){
+        if(start<end){
+            int mid=start+(end-start)/2;
+            mergeSort(arr,start,mid);
+            mergeSort(arr,mid+1,end);
+            merge(arr,start,mid,end);
+        }
+    }
+    public static void merge(int[] arr,int start,int mid, int end){
+      int i =start;
+      int j=mid+1;
+      int k =0;
+      int[] temp=new int[end-start+1];
+      while(i<=mid&&j<=end){
+        if(arr[i]<=arr[j])
+          temp[k++]=arr[i++];
+        else 
+          temp[k++]=arr[j++];
+      }
+
+      while(i<=mid){
+        temp[k++]=arr[i++];
+      }
+      while(j<=end){
+        temp[k++]=arr[j++];
+      }
+      for(i =start,k=0;i<=end;i++,k++){
+        arr[i]=temp[k];
+      }
+    }
+}
