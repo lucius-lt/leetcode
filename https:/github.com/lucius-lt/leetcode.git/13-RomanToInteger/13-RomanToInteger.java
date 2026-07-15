c276406936a7ba4ea1e34d537205c4825fc2cbd7
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:12:45 +0530

    Create 13-RomanToInteger

---
+++ https:/github.com/lucius-lt/leetcode.git/13-RomanToInteger/13-RomanToInteger.java
@@ -0,0 +1,23 @@
+// Last updated: 7/16/2026, 1:12:44 AM
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+          map.put('I', 1);
+          map.put('V', 5);
+          map.put('X', 10);
+          map.put('L', 50);
+          map.put('C', 100);
+          map.put('D', 500);
+          map.put('M', 1000);
+          int result=0;
+          int prev=0;
+          for(int i =s.length()-1;i>=0;i--){
+            int curr=map.get(s.charAt(i));
+            if(curr<prev) result-=curr;
+            else result+=curr;
+            prev=curr;
+          }
+          return result;
+    }
+}
