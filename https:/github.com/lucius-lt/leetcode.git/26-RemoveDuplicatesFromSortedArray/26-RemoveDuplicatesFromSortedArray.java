1a57eaa5ec10a4b59650064cb5ae5cece99af8bc
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:12:32 +0530

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ https:/github.com/lucius-lt/leetcode.git/26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,15 @@
+// Last updated: 7/16/2026, 1:12:31 AM
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums==null||nums.length==0) return 0;
+       int k=0;
+       for(int i =1;i<nums.length;i++){
+        if(nums[i]!=nums[k]){
+             k++;
+            nums[k]=nums[i];
+           
+             }
+       }
+       return k+1;
+    }
+}
