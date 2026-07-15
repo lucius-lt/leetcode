df5ee6c648b00d6fa830886cd54ce29cc2a395bb
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:08:08 +0530

    Create 2113-FindTheKthLargestIntegerInTheArray

---
+++ https:/github.com/lucius-lt/leetcode.git/2113-FindTheKthLargestIntegerInTheArray/2113-FindTheKthLargestIntegerInTheArray.java
@@ -0,0 +1,20 @@
+// Last updated: 7/16/2026, 1:08:08 AM
+class Solution {
+    public String kthLargestNumber(String[] nums, int k) {
+        PriorityQueue<String> pq=new PriorityQueue<>((a,b)->{
+            if(a.length()!=b.length()){
+                return a.length()-b.length();
+            }
+            return a.compareTo(b);
+        });
+
+
+        for(String num:nums){
+            pq.offer(num);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
