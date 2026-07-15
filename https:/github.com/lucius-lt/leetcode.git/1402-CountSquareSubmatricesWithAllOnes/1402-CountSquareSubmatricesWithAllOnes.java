220641025778351cf3cc3d77338ab44ae1bb63f7
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:08:37 +0530

    Create 1402-CountSquareSubmatricesWithAllOnes

---
+++ https:/github.com/lucius-lt/leetcode.git/1402-CountSquareSubmatricesWithAllOnes/1402-CountSquareSubmatricesWithAllOnes.java
@@ -0,0 +1,26 @@
+// Last updated: 7/16/2026, 1:08:36 AM
+class Solution {
+    public int countSquares(int[][] matrix) {
+       int m = matrix.length,n=matrix[0].length;
+       int[][] dp=new int[m][n];
+       int total=0;
+       for(int i =0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(matrix[i][j]==1){
+                if(i==0||j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i][j-1],dp[i-1][j]));
+                }
+            }
+            else {
+                dp[i][j]=0;
+            }
+             total+=dp[i][j];
+        }
+       
+       }
+       return total; 
+    }
+}
