351464b4b811e4d6d0fe12b710afab95796591c0
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:12:07 +0530

    Create 50-PowxN

---
+++ https:/github.com/lucius-lt/leetcode.git/50-PowxN/50-PowxN.java
@@ -0,0 +1,17 @@
+// Last updated: 7/16/2026, 1:12:07 AM
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        while (N > 0) {
+            if (N % 2 == 1) {
+                ans = ans * x;
+            }
+            x = x * x;
+            N = N / 2;
+        }
+        return ans;}}
