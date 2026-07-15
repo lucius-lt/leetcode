33b3faacf383ad62821f02c25ea6f78300c12ca0
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:08:07 +0530

    Create 2277-CountEqualAndDivisiblePairsInAnArray

---
+++ https:/github.com/lucius-lt/leetcode.git/2277-CountEqualAndDivisiblePairsInAnArray/2277-CountEqualAndDivisiblePairsInAnArray.java
@@ -0,0 +1,15 @@
+// Last updated: 7/16/2026, 1:08:06 AM
+class Solution {
+    public int countPairs(int[] nums, int k) {
+        int n = nums.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j =i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    if((i*j)%k==0) count++;
+                }
+            }
+        }
+        return count;
+    }
+}
