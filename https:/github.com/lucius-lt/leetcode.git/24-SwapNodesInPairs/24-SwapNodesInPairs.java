821ad350aa4494591af5940715f122655895d065
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:12:33 +0530

    Create 24-SwapNodesInPairs

---
+++ https:/github.com/lucius-lt/leetcode.git/24-SwapNodesInPairs/24-SwapNodesInPairs.java
@@ -0,0 +1,33 @@
+// Last updated: 7/16/2026, 1:12:33 AM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        
+        ListNode dummy=new ListNode(0);
+        ListNode prev = dummy;
+        dummy.next=head;
+
+        while (prev.next!=null&&prev.next.next!=null){
+               ListNode first = prev.next;
+               ListNode second = first.next;
+
+               first.next=second.next;
+
+               prev.next= second;
+
+               second.next = first;
+
+               prev=first;
+        }
+        return dummy.next;
+    }
+}
