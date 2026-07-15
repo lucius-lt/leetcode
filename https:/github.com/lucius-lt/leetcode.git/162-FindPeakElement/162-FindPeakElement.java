25db9e79a59759b53513e94368ae955575053e23
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:10:43 +0530

    Create 162-FindPeakElement

---
+++ https:/github.com/lucius-lt/leetcode.git/162-FindPeakElement/162-FindPeakElement.java
@@ -0,0 +1,17 @@
+// Last updated: 7/16/2026, 1:10:43 AM
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
