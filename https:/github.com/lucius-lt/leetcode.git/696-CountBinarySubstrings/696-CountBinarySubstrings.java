c781f736492df0dda3f998e4e2d3ca608a84d257
Niyati! <143438164+lucius-lt@users.noreply.github.com>
2026-07-16 01:09:10 +0530

    Create 696-CountBinarySubstrings

---
+++ https:/github.com/lucius-lt/leetcode.git/696-CountBinarySubstrings/696-CountBinarySubstrings.java
@@ -0,0 +1,21 @@
+// Last updated: 7/16/2026, 1:09:10 AM
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int prev=0;
+        int curr=1;
+        int count=0;
+        for(int i =1;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                curr++;
+            }
+            else{
+                count+=Math.min(prev,curr);
+                prev=curr;
+                curr=1;
+            }
+           
+        }
+         count+=Math.min(prev,curr);
+        return count;                  
+    }
+}
